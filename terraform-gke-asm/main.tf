1cac7ddff879d399ee198cfe28bcd145e5860def
lionelsakoue <130628424+lionelsakoue@users.noreply.github.com>
2025-09-26 16:08:48 -0400

    Update main.tf

--- terraform-gke-asm/main.tf
+++ terraform-gke-asm/main.tf
@@ -6,16 +6,36 @@ module "cluster" {
   cluster_name = var.cluster_name
 }
 
+# Wait for cluster + nodes to be Ready
+resource "null_resource" "gke_ready" {
+  depends_on = [module.cluster]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud container clusters get-credentials ${module.cluster.cluster_name} \
+        --zone ${var.zone} \
+        --project ${var.project_id}
+
+      # Wait until all nodes are Ready
+      kubectl wait --for=condition=Ready nodes --all --timeout=300s
+    EOT
+  }
+}
+
 module "asm" {
   source        = "../modules/asm"
   namespace     = "istio-system"
   istio_version = "1.22.0"
+
+  depends_on = [null_resource.gke_ready]
 }
 
 module "ingress" {
-  source     = "../modules/ingress"
+  source       = "../modules/ingress"
   cluster_name = module.cluster.cluster_name
   namespace    = "istio-system"
+
+  depends_on = [module.asm]
 }
 
 # Root-level outputs
@@ -34,3 +54,4 @@ output "asm_namespace" {
 output "ingress_namespace" {
   value = module.ingress.ingress_gateway_namespace
 }
+
