ca536143c808cdcf95996171dfd8b691b840df9f
lionelsakoue <130628424+lionelsakoue@users.noreply.github.com>
2026-04-05 22:38:48 -0400

    Change GKE membership location to global

--- config-synch/main.tf
+++ config-synch/main.tf
@@ -1,7 +1,8 @@
 resource "google_gke_hub_feature_membership" "acm_gke_ha" {
-  location   = "us-central1"
+  location   = "global"
   feature    = "configmanagement"
   membership = "gke-ha-cluster"
+  membership_location = "us-central1"
 
   configmanagement {
     config_sync {
