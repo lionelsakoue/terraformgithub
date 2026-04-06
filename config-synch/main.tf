c18ba0216f003af970b00a797d52a8a108be739e
lionelsakoue <130628424+lionelsakoue@users.noreply.github.com>
2026-04-05 22:19:47 -0400

    Remove GitHub token and Config Sync resources
    Removed GitHub token secret management and Kubernetes resources for Config Sync.

--- config-synch/main.tf
+++ config-synch/main.tf
@@ -1,46 +1,11 @@
-# 🔥 Read GitHub token from Secret Manager
-data "google_secret_manager_secret_version" "github_token" {
-  secret  = "github-token"
-  version = "latest"
-}
+resource "google_gke_hub_feature_membership" "acm_gke_ha" {
+  location   = "us-central1"
+  feature    = "configmanagement"
+  membership = "gke-ha-cluster"
 
-# 🔥 Kubernetes secret for Config Sync
-resource "kubernetes_secret_v1" "git_creds" {
-  metadata {
-    name      = "git-creds"
-    namespace = "config-management-system"
-  }
-
-  data = {
-    username = "lionelsakoue"
-    token    = data.google_secret_manager_secret_version.github_token.secret_data
-  }
-
-  type = "Opaque"
-}
-
-# 🔥 RootSync resource
-resource "kubernetes_manifest" "rootsync" {
-  manifest = {
-    apiVersion = "configsync.gke.io/v1beta1"
-    kind       = "RootSync"
-    metadata = {
-      name      = "root-sync"
-      namespace = "config-management-system"
-    }
-    spec = {
-      sourceType   = "git"
-      sourceFormat = "unstructured"
-      git = {
-        repo     = "https://github.com/lionelsakoue/shinner-deployment.git"
-        branch   = "dev"
-        revision = "HEAD"
-        dir      = "overlays/shinner-dev"
-        auth     = "token"
-        secretRef = {
-          name = "git-creds"
-        }
-      }
+  configmanagement {
+    config_sync {
+      enabled = true
     }
   }
 }
